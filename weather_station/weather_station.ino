2e0c6c33356fb125e0ffa78dbf515f74b1e5d7d6
Mridul0001 <mridulmahajan0001@gmail.com>
2019-03-23 13:31:14 +0530

    Weather station code completed

--- weather_station/weather_station.ino
+++ weather_station/weather_station.ino
@@ -1,9 +1,237 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h> 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
 
+
+// Definig variables
+SFE_BMP180 pressure;
+SoftwareSerial espSerial(2, 3);
+#define ALTITUDE 306.0
+
+#define DEBUG true
+#define DHTPIN 4
+#define DHTTYPE    DHT22  // DHT 22 (AM2302)
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+String mySSID = "ASUS_M1";       // WiFi SSID
+String myPWD = "pokemon@1234"; // WiFi Password
+String myAPI = "HYG5IQE0EQYTWW3N";   // API Key
+String myHOST = "api.thingspeak.com";
+String myPORT = "80";
+String myFIELD = "field1"; 
+double temp;
+// Rain Sensor
+
+//Setup method
+void setup()
+
+{
+  Serial.begin (9600);
+  espSerial.begin(9600);
+
+  pinMode (7 , INPUT);//rain digital;
+  
+  espData("AT+RST", 1000, DEBUG);                      //Reset the ESP8266 module
+  espData("AT+CWMODE=1", 1000, DEBUG);                 //Set the ESP mode as station mode
+  espData("AT+CWJAP=\""+ mySSID +"\",\""+ myPWD +"\"", 1000, DEBUG);   //Connect to WiFi network
+  delay(1000);
+  
+  //BMP
+
+  Serial.println("REBOOT");
+
+  // Initialize the sensor (it is important to get calibration values stored on the device).
+
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
+
+  //DHT
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
 
+{
+  //rain
+  int value = analogRead(A1);
+  int digital = digitalRead(7);
+
+ 
+  Serial.println (value);
+  Serial.println (digital);
+
+  //dht
+
+   delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    temp=event.temperature;
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  //BMP
+
+   char status;
+  double T,P,p0,a;
+
+  // Loop here getting pressure readings every 10 seconds.
+  
+  Serial.println();
+  Serial.print("provided altitude: ");
+  Serial.print(ALTITUDE,0);
+  Serial.print(" meters, ");
+  Serial.print(ALTITUDE*3.28084,0);
+  Serial.println(" feet");
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    delay(status);
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+   
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+          a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(5000);  // Pause for 5 seconds.
+
+
+   //ESP send to thingspeak
+    String sendData = "GET /update?api_key="+ myAPI +"&"+ myFIELD +"="+String(temp)+"&field2=" +String(event.relative_humidity)
+    +"&field3=" +String(P)+"&field4=" +String(digital)+"&field5=" +String(p0);
+    espData("AT+CIPMUX=1", 1000, DEBUG);       //Allow multiple connections
+    espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
+    espData("AT+CIPSEND=0," +String(sendData.length()+4),1000,DEBUG);  
+    espSerial.find(">"); 
+    espSerial.println(sendData);
+    Serial.print("Value to be sent: ");
+    Serial.println(temp);Serial.print(", ");Serial.print(event.relative_humidity);
+    Serial.print(", ");Serial.print(P);
+    Serial.print(", ");Serial.print(digital);
+    Serial.print(", ");Serial.print(p0);
+     
+    espData("AT+CIPCLOSE=0",1000,DEBUG);
+    delay(10000);
 }
+  String espData(String command, const int timeout, boolean debug)
+  {
+    Serial.print("AT Command ==> ");
+    Serial.print(command);
+    Serial.println("     ");
+    
+    String response = "";
+    espSerial.println(command);
+    long int time = millis();
+    while ( (time + timeout) > millis())
+    {
+      while (espSerial.available())
+      {
+        char c = espSerial.read();
+        response += c;
+      }
+    }
+    if (debug)
+    {
+      //Serial.print(response);
+    }
+    return response;
+ }
